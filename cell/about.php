f37aa98ee6ae10ce72934b5f0eb9f5336b688b3d
Prashantmishragkp <pmishrajiup53@gmail.com>
2025-08-02 21:06:35 +0530

    Delete cell directory

--- cell/about.php
+++
@@ -1,8 +0,0 @@
-<?php include('includes/header.php'); ?>
-<section>
-  <div class="container">
-    <h2>About BIT E-Cell</h2>
-    <p>BIT E-Cell fosters a culture of entrepreneurship and innovation at Buddha Institute of Technology, GIDA, Gorakhpur. Through events, mentorship, and incubation, we support student-led startups.</p>
-  </div>
-</section>
-<?php include('includes/footer.php'); ?>
